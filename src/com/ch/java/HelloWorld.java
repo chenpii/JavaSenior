71ffe721617df7918ba69a73ce8700cb13082096
chenh <641760512@qq.com>
2021-12-06 23:17:30 +0800

    test

--- src/com/ch/java/HelloWorld.java
+++ src/com/ch/java/HelloWorld.java
@@ -3,5 +3,6 @@ package com.ch.java;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
     }
 }
