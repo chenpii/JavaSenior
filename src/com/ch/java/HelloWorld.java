e2bcb751649ebb6da891dce064dfdf0fe2d743d6
chenh <641760512@qq.com>
2021-12-08 09:42:38 +0800

    线程的创建方式一：继承Thread类

--- src/com/ch/java/HelloWorld.java
+++ src/com/ch/java/HelloWorld.java
@@ -2,7 +2,6 @@ package com.ch.java;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
+        System.out.println("hello world");
     }
 }
