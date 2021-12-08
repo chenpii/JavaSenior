d1cc89c011266d1843315dec9437547198223057
chenh <641760512@qq.com>
2021-12-08 14:46:44 +0800

    线程的实现方式一：继承thread类 匿名类

---
+++ src/com/ch/java/thread/exer/ThreadDemo.java
@@ -0,0 +1,56 @@
+package com.ch.java.thread.exer;
+
+/**
+ * 练习：创建两个分线程，其中一个线程遍历100以内的偶数，另一个线程遍历100以内的奇数
+ *
+ * @author chenpi
+ * @create 2021-12-08 14:24
+ */
+public class ThreadDemo {
+    public static void main(String[] args) {
+        OddThread oddThread = new OddThread();
+        oddThread.start();
+        EvenThread evenThread = new EvenThread();
+        evenThread.start();
+
+        //创建Thread匿名子类方式
+        new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    if (i % 2 == 0) {
+                        System.out.println(Thread.currentThread().getName() + "匿名类" + i);
+                    }
+                }
+            }
+        }.start();
+    }
+}
+
+/**
+ * 遍历奇数的线程
+ */
+class OddThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 != 0) {
+                System.out.println(Thread.currentThread().getName() + "奇数线程：" + i);
+            }
+        }
+    }
+}
+
+/**
+ * 遍历偶数线程
+ */
+class EvenThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(Thread.currentThread().getName() + "偶数线程：" + i);
+            }
+        }
+    }
+}
--- src/com/ch/java/thread/java/HelloWorld.java
+++ src/com/ch/java/thread/java/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.ch.java;
+package com.ch.java.thread.java;
 
 public class HelloWorld {
     public static void main(String[] args) {
