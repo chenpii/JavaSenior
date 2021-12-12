c1e391d2b56d0f26098c8116a868a4705002303e
chenh <641760512@qq.com>
2021-12-12 22:25:35 +0800

    线程安全的练习

---
+++ 02.Thread/src/com/ch/exer/AccountTest.java
@@ -0,0 +1,72 @@
+package com.ch.exer;
+
+/**
+ * 银行有一个账户
+ * 有两个储户分别向同一个账户存3000元，每次存1000，存3次。每次存完打印账户余额。
+ * <p>
+ * 分析：
+ * 1.是否是多线程问题？是，两个储户线程
+ * 2.是否有共享数据？有，账户的余额
+ * 3.是否有线程安全问题？有
+ * 4.需要考虑如何解决线程安全问题？同步机制：有三种方式。
+ *
+ * @author chenpi
+ * @create 2021-12-12 22:04
+ */
+
+class Account {
+    private double balance;
+
+    public Account() {
+    }
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    //存钱
+    public synchronized void deposit(double amt) {
+        if (amt > 0) {
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+            balance += amt;
+            System.out.println(Thread.currentThread().getName()+":存钱成功。余额为" + balance);
+        }
+    }
+}
+
+class Customer extends Thread {
+    private Account acct;
+
+    public Customer(Account acct) {
+        this.acct = acct;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            acct.deposit(1000);
+        }
+    }
+}
+
+public class AccountTest {
+    public static void main(String[] args) {
+        Account acct = new Account();
+
+        Customer c1 = new Customer(acct);
+        Customer c2 = new Customer(acct);
+
+        c1.setName("甲");
+        c2.setName("乙");
+
+        c1.start();
+        c2.start();
+    }
+}
