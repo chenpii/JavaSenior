fb1e0661bfba52a8bde4a6282f27a5a542c59b6e
chenh <641760512@qq.com>
2021-12-19 19:24:48 +0800

    String、StringBuffer、StringBuilder效率对比

--- 03.String/src/com/ch/java/StringBufferBuilderTest.java
+++ 03.String/src/com/ch/java/StringBufferBuilderTest.java
@@ -9,6 +9,34 @@ import org.junit.Test;
  * @create 2021-12-19 15:40
  */
 public class StringBufferBuilderTest {
+
+    /*
+    String、StringBuffer、StringBuilder效率对比
+     */
+    @Test
+    public void test3() {
+
+    }
+    /*
+    StringBuffer append(xxx)：字符串拼接。
+    StringBuffer delete(int start,int end)：删除指定位置的内容。
+    StringBuffer replace(int start,int end,String str)：把[start,end)的位置替换成str。
+    StringBuffer insert(int offset,xxx)：在指定位置插入xxx。
+    StringBuffer reverse()：当前字符串逆转。
+     */
+    @Test
+    public void test2() {
+        StringBuffer sb1 = new StringBuffer("abc");
+        sb1.append(1);
+        sb1.append('1');
+        System.out.println(sb1);//abc11
+//        sb1.delete(2, 4);
+//        sb1.replace(2, 4,"hello");
+        sb1.insert(3,"hello");//abchello11
+        System.out.println(sb1);
+        System.out.println(sb1.reverse());//11ollehcba
+
+    }
     /*
     String、StringBuffer、StringBuilder三者的异同？
     String：不可变的字符序列。底层使用char[]存储。
