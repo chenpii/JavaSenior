2409af351c9b375fa9a4833f98116b6921e3fe2c
chenh <641760512@qq.com>
2022-01-12 20:54:48 +0800

    localdatetime类的获取、设置、加减方法

--- 04.String/src/com/ch/java/JDK8DateTime.java
+++ 04.String/src/com/ch/java/JDK8DateTime.java
@@ -32,8 +32,7 @@ public class JDK8DateTime {
         LocalDateTime localDateTime1 = LocalDateTime.of(2022, 10, 1, 13, 23, 13);
         System.out.println(localDateTime1);
 
-
-        //获取操作：getXxxx()方法
+        //获取相关属性：getXxxx()方法
         //当前时间2021-01-12 20:42，周三
         System.out.println(localDateTime.getDayOfMonth());//获取本月的第几天--12
         System.out.println(localDateTime.getDayOfWeek());//获取本周的第几天--WEDNESDAY
@@ -43,7 +42,22 @@ public class JDK8DateTime {
         System.out.println(localDateTime.getHour());//20
         System.out.println(localDateTime.getMinute());//42
 
-        //设置操作:withXxxx()方法
+        //设置相关属性:withXxxx()方法 体现不可变性
+        LocalDateTime localDateTime2 = localDateTime.withDayOfMonth(22);//设置成22号
+        System.out.println(localDateTime);
+        System.out.println(localDateTime2);
+
+        LocalDateTime localDateTime3 = localDateTime.withHour(4);//设置成4点
+        System.out.println(localDateTime);
+        System.out.println(localDateTime3);
+
+        //加减操作 plusXxx()、minusXxx()
+        LocalDateTime localDateTime4 = localDateTime.plusMonths(3);//加上3个月
+        System.out.println(localDateTime);
+        System.out.println(localDateTime4);
+        LocalDateTime localDateTime5 = localDateTime.minusDays(1);//减1天
+        System.out.println(localDateTime);
+        System.out.println(localDateTime5);
 
 
     }
