278d7c34319ff5dfa119a4673e26ff39095dabd8
chenh <641760512@qq.com>
2022-02-14 17:44:28 +0800

    enum枚举类实现接口

--- 05.Enum/src/com/ch/java/SeasonTest1.java
+++ 05.Enum/src/com/ch/java/SeasonTest1.java
@@ -17,6 +17,7 @@ public class SeasonTest1 {
         Season1[] values=Season1.values();
         for (int i = 0; i < values.length; i++) {
             System.out.println(values[i]);
+            values[i].show();
         }
 
         //valueof(String objName):返回枚举类中对象名是objName的对象；若没有，则抛异常：IllegalArgumentException
@@ -25,13 +26,37 @@ public class SeasonTest1 {
 
     }
 }
+interface show{
+    void show();
+}
+
 //使用enum关键字定义枚举类
-enum Season1 {
+enum Season1 implements show{
     //1.提供当前枚举类的对象，多个对象之间用","分开，末尾对象";"结束
-    SPRING("春天", "春暖花开"),
-    SUMMER("夏天", "夏日炎炎"),
-    AUTUMN("秋天", "秋高气爽"),
-    WINTER("冬天", "寒冬腊月");
+    SPRING("春天", "春暖花开"){
+        @Override
+        public void show() {
+            System.out.println("春天在哪里？");
+        }
+    },
+    SUMMER("夏天", "夏日炎炎"){
+        @Override
+        public void show() {
+            System.out.println("宁夏");
+        }
+    },
+    AUTUMN("秋天", "秋高气爽"){
+        @Override
+        public void show() {
+            System.out.println("秋天不回来");
+        }
+    },
+    WINTER("冬天", "寒冬腊月"){
+        @Override
+        public void show() {
+            System.out.println("大约在冬季");
+        }
+    };
 
     //2.声明Seanson对象的属性
     private final String seasonName;
@@ -52,14 +77,5 @@ enum Season1 {
     public String getSeasonDesc() {
         return seasonDesc;
     }
-    //4.其他诉求2：提供toString方法
-
-//    @Override
-//    public String toString() {
-//        return "Season{" +
-//                "seasonName='" + seasonName + '\'' +
-//                ", seasonDesc='" + seasonDesc + '\'' +
-//                '}';
-//    }
 
 }
