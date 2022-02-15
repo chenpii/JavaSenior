e46ca1e3b6a1bcbf32c3f3303cbe9c01e6ea69be
chenh <641760512@qq.com>
2022-02-15 16:53:57 +0800

    STATUS类改成枚举类

--- myproject03/src/com/ch/team/service/Status.java
+++ myproject03/src/com/ch/team/service/Status.java
@@ -1,29 +1,32 @@
 package com.ch.team.service;
 
 /**
- * 
  * @author chenpi
  * @Description 表示员工的状态。枚举类:只有有限个对象。
  * @date 2021年2月12日
  */
-public class Status {
-	private final String NAME;
+//public class Status {
+//	private final String NAME;
+//
+//	public Status(String name) {
+//		super();
+//		this.NAME = name;
+//	}
+//
+//	public static final Status FREE = new Status("FREE");
+//	public static final Status BUSY = new Status("BUSY");
+//	public static final Status VOCATION = new Status("VOCATION");
+//
+//	public String getNAME() {
+//		return NAME;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return NAME;
+//	}
+//}
+public enum Status {
+    FREE, BUSY, VOCATION;
 
-	public Status(String name) {
-		super();
-		this.NAME = name;
-	}
-
-	public static final Status FREE = new Status("FREE");
-	public static final Status BUSY = new Status("BUSY");
-	public static final Status VOCATION = new Status("VOCATION");
-
-	public String getNAME() {
-		return NAME;
-	}
-
-	@Override
-	public String toString() {
-		return NAME;
-	}
 }
