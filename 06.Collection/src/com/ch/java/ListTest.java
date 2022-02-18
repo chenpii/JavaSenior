26c0f88380323e275521e221772f22a3c541f7f5
chenh <641760512@qq.com>
2022-02-18 17:32:55 +0800

    List接口测试方法

---
+++ 06.Collection/src/com/ch/java/ListTest.java
@@ -0,0 +1,17 @@
+package com.ch.java;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * @author chenpi
+ * @create 2022-02-18 15:21
+ */
+public class ListTest {
+
+    @Test
+    public void arrayListTest(){
+        ArrayList arrayList = new ArrayList();
+    }
+}
