fa7df488947b9e8046953b9e139222e9ae3536f4
chenh <641760512@qq.com>
2022-03-02 22:56:40 +0800

    缓冲流的练习-图片加密

---
+++ 09.IO/src/com/ch/exer/PicTest.java
@@ -0,0 +1,18 @@
+package com.ch.exer;
+
+import org.junit.Test;
+
+/**
+ * @author chenpi
+ * @create 2022-03-02 22:43
+ */
+public class PicTest {
+
+    /**
+     * 图片的加密
+     */
+    @Test
+    public void test1() {
+        
+    }
+}
