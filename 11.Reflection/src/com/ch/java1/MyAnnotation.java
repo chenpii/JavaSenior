203644ef86c292cdf79eb64a9661e6c86c2869ef
chenh <641760512@qq.com>
2022-03-21 17:31:10 +0800

    丰富Person类结构

--- 11.Reflection/src/com/ch/java1/MyAnnotation.java
+++ 11.Reflection/src/com/ch/java1/MyAnnotation.java
@@ -1,8 +1,18 @@
 package com.ch.java1;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+
 /**
  * @author chenpi
  * @create 2022-03-21 16:58
  */
+@Target({TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE})
+@Retention(RetentionPolicy.SOURCE)
 public @interface MyAnnotation {
+    String value();
 }
